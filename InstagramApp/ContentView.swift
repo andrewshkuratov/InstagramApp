971e5b759f2de7bf5c991296f50b18de802633a5
Andrew <andrea.awesome.ua@gmail.com>
2020-07-12 09:25:03 +0300

    Initial Commit

---
+++ InstagramApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramApp
+//
+//  Created by Andrew on 12.07.2020.
+//  Copyright © 2020 com.andrewShkuratov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
